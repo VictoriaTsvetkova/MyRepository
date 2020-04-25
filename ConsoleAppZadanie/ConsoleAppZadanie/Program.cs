90f22d1433ecdbcdd18aa5ccf160cacee02fcad1
Victoria <vtsvietkova@list.ru>
2020-04-25 12:17:31 +0300

    Описание класса

--- ConsoleAppZadanie/ConsoleAppZadanie/Program.cs
+++ ConsoleAppZadanie/ConsoleAppZadanie/Program.cs
@@ -13,6 +13,38 @@ namespace ConsoleAppZadanie
 			public string Surname = "";
 			public int Semester = 0;
 		}
+		static class Plan
+		{
+			static public int Size;
+			static public List<Subject> SubjectSemester = new List<Subject>();
+			static public void SubjectItems()
+			{
+				Console.Write("Размер массива = ");
+				Size = Convert.ToInt32(Console.ReadLine());
+				for (int i = 0; i < Size; i++)
+				{
+					Subject TempSubject = new Subject();
+					Console.Write("Название предмета №" + i + " = ");
+					TempSubject.Name = Console.ReadLine().ToString();
+					Console.Write("Фамилия преподавателя №" + i + " = ");
+					TempSubject.Surname = Console.ReadLine().ToString();
+					bool hasLetters;
+					do
+					{
+						Console.Write("Семестр №" + i + " = ");
+						string t = Console.ReadLine();
+						hasLetters = t.AsEnumerable().Any(ch => char.IsLetter(ch));
+						if (!hasLetters)
+							TempSubject.Semester = Convert.ToInt32(t);
+						else
+						{
+							Console.Write("Номер семестра не должен содержать буквы. Только цифры. Повторите ввод номера семестра №" + i + " = ");
+						}
+					}
+					while (hasLetters);
+					SubjectSemester.Add(TempSubject);
+				}
+			}
 			static void Main(string[] args)
 			{
 			}
