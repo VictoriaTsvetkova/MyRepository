0c9b9a8c45eb62f163d5964ac969ef660786c75a
Victoria <vtsvietkova@list.ru>
2020-04-25 12:12:41 +0300

    Создание Subject

---
+++ ConsoleAppZadanie/ConsoleAppZadanie/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace ConsoleAppZadanie
+{
+	class Program
+	{
+		class Subject
+		{
+			public string Name = "";
+			public string Surname = "";
+			public int Semester = 0;
+		}
+			static void Main(string[] args)
+			{
+			}
+		}
+	}
+}
